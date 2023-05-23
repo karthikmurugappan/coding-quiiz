1bc4c34f0dcc5a4fff1a1b0dd230e129fc8e6955
karthikmurugappan <karthikmurugappan7@gmail.com>
2023-05-23 18:07:05 -0400

    created files and added questions

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var questions = [{
+    question: "What is the basic difference between JavaScript and Java?",
+    choices: ["Functions are considered as fields", "Functions are values, and there is no hard distinction between methods and fields", "Variables are specific", "There is no difference"],
+    answer: 1
+},
+{
+    question: "Which of the following can be used to call a JavaScript Code Snippet?",
+    choices: ["Function/Method", "Preprocessor", "Triggering Event", "RMI"],
+    answer: 0
+},
+{
+    question: "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+    choices: ["Position", "Window", "Location", "Standard"],
+    answer: 1
+}
+{
+    question: "Which of the following is not javascript data types?",
+    choices: ["Null", "Undefined", "Number", "All of the mentioned"],
+    answer: 3
+},
+]
